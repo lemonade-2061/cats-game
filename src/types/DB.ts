373f97c4ec4ba36ab71a7c35fb25b508e1c89c61
lemonade-2061 <lemonenergy.2061.7.29@gmail.com>
2026-02-05 12:52:32 +0900

    feat:構造体の作成

---
+++ src/types/DB.ts
@@ -0,0 +1,27 @@
+export interface Cat {
+    id: string;
+    name: string; 
+    coatColor: string;
+    age: number;
+    created_at: string;
+    birthday: string;
+}
+
+export interface User {
+    id: string;
+    email: string;
+    password: string;
+    created_at: string;
+}
+
+export interface GameRecord {
+    id: string;
+    user_id: string;
+    cat_id: string;
+    created_at: string;
+    money: number;
+    hairball_count: number;
+    total_hairball: number;
+    days_played: number;
+}
+
