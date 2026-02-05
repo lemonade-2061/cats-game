f90efeffd15ec58d0522f5b17651658c1884e8e3
hayao <mihokubota@outlook.jp>
2026-02-05 14:42:39 +0900

    新規登録ページを作った

--- src/app/newAccount/page.tsx
+++ src/app/newAccount/page.tsx
@@ -4,38 +4,38 @@ import { useState } from "react";
 import { supabase } from "@/lib/supabase";
 import { useRouter } from "next/navigation";
 
-export default function LoginPage() {
+export default function NewAccountPage() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
     const router = useRouter();
 
-    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    const signUp = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         setLoading(true);
 
-        const { error } = await supabase.auth.signInWithPassword({
+        const { error } = await supabase.auth.signUp({
             email,
             password,
         });
 
         if (error) alert(error.message);
-        else router.push("/care");
+        else router.push("/login");
         setLoading(false);
     };
 
-    const EnterNewAccountPage = async () => {
-        router.push("/newAccount");
+    const enterLoginPage = async () => {
+        router.push("/login");
     };
 
     return (
         <main className="ninshou-background">
-            
+
             <form
-                onSubmit={handleLogin}
+                onSubmit={signUp}
                 className="login-form"
             >
-                <h1 style={{ margin: 0, textAlign: "center", fontSize: "28px" }}>ログイン</h1>
+                <h1 style={{ margin: 0, textAlign: "center", fontSize: "28px" }}>新規登録</h1>
 
                 <input
                     type="email"
@@ -56,11 +56,11 @@ export default function LoginPage() {
                 />
 
                 <button type="submit" disabled={loading} style={{ padding: 12, borderRadius: 8, cursor: "pointer" }}>
-                    {loading ? "読み込み中..." : "ログイン"}
+                    {loading ? "読み込み中..." : "新規登録"}
                 </button>
 
-                <button type="button" onClick={EnterNewAccountPage} style={{ padding: 12, borderRadius: 8, cursor: "pointer" }}>
-                    新規登録へ
+                <button type="button" onClick={enterLoginPage} style={{ padding: 12, borderRadius: 8, cursor: "pointer" }}>
+                    ログインへ
                 </button>
             </form>
         </main>
