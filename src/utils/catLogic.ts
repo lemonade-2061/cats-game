0280fe96c2fe8d67167638b9271f2df094b927bb
lemonade-2061 <lemonenergy.2061.7.29@gmail.com>
2026-02-06 05:10:47 +0900

    feat:猫と売買のロジック

--- src/utils/catLogic.ts
+++ src/utils/catLogic.ts
@@ -1,18 +1,17 @@
 import { Cat, GameRecord } from "@/types/DB";
 
-export const createCat = (name: string, coartColor: string): Cat => {
+export const createCat = (name: string, coatColor: string): Cat => {
     return {
         id: crypto.randomUUID(),
         name: name,
-        coatColor: coartColor,
         age: 0,
         created_at: new Date().toISOString(),
         birthday: new Date().toISOString(),
     };
 };
-
+const HAIRBALL_INCREMENT = 3;
 export const getHairball = (gameRecord: GameRecord): number => {
-    return gameRecord.hairball_count += 3;
+  return gameRecord.hairball_count + HAIRBALL_INCREMENT;
 }
 
 
--- src/utils/priceLogic.ts
+++ src/utils/priceLogic.ts
@@ -3,8 +3,9 @@ import { GameRecord, Item } from "@/types/DB"
 type Result = { success: true; data: GameRecord } | { success: false; message: string };
 
 export const addMoney = (gameRecord: GameRecord): Result => {
+    const HAIRBALL_INCREMENT = 10
     if (gameRecord.hairball_count < 10) {
-        return { success: false, message: "毛玉が足りないよ！" };
+        return { success: false, message: `毛玉が${HAIRBALL_INCREMENT - gameRecord.hairball_count}個足りないよ！` };
     }
     return {
         success: true,
@@ -20,8 +21,6 @@ export const buyItem = (gameRecord: GameRecord, item: Item): Result => {
     if (gameRecord.money < item.price) {
         return { success: false, message: "お金がないよ！" };
     }
-
-
     return {
         success: true,
         data: {
